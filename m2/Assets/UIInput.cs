6b10405f6384f84991bbc6459b6937790e6ca465
ylw2011 <57453886+ylw2011@users.noreply.github.com>
2020-04-15 12:18:55 +0800

    200415

--- m2/Assets/UIInput.cs
+++ m2/Assets/UIInput.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 
 public class UIInput : MonoBehaviour
 {
+    Text[] mes;
+
     public void buttonstart()
     {
         SceneManager.LoadScene(1);
@@ -18,7 +20,12 @@ public class UIInput : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (Application.platform == RuntimePlatform.WindowsEditor) Debug.Log("開發中");
+
+        mes = GameObject.Find("Canvas").GetComponentsInChildren<Text>();
+
+        string[] text = System.IO.File.ReadAllLines("Assets/tw.txt");
+        for (int i = 0; i < text.Length; i++) mes[i].text=text[i];
     }
 
     // Update is called once per frame
